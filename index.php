ed90ba1189129254a55a47c0294be0848e05af2b
Jen Shin <jshin13@mail.greenriver.edu>
2018-04-13 15:51:29 -0400

    loops to access json obj - unsuccessful

--- index.php
+++ index.php
@@ -24,24 +24,72 @@
 
     // home route
     $f3->route('GET|POST /', function($f3) {
-        if(isset($_POST)) {
-            var_dump($_POST);
-        }
-        $isValid = true;
-        if(!empty($_POST['title'])) {
-            $title = $_POST['title'];
-        } else {
-            $isValid = false;
-        }
-        if(!empty($_POST['new-post'])) {
-            $content = $_POST['new-post'];
-            $f3->set('content', $content);
-        } else {
-            $isValid = false;
-        }
 
-        if($isValid) {
-            $f3->reroute('/view-post');
+        if(isset($_POST['submit'])) {
+            $contentErr = "";
+            $titleErr = "";
+           // var_dump($_POST);
+            // }
+            $isValid = true;
+
+            if (!empty($_POST['title'])) {
+                $title = $_POST['title'];
+            } else {
+                $titleErr = "Please input a title.";
+                $f3->set('titleErr', $titleErr);
+                $isValid = false;
+            }
+            $content = $_POST['new-post'];
+            $json = json_decode($content, true);
+            print_r($content.'<br/>');
+
+            foreach($json as $key => $value) {
+                print_r($value);
+                foreach($value as $valuekey => $secondvalue) {
+                    print_r($secondvalue);
+                    foreach($secondvalue as $key1 => $value2) {
+                        if (!isset($value2)) {
+                            $isValid = false;
+
+                            $contentErr = "Please input text and/or images.";
+                            $f3->set('contentErr', $contentErr);
+                            return;
+                        }
+                    }
+                }
+            }
+
+            /*foreach($json['ops']['insert'] as $item) {
+                if($item == "\n") {
+                    $isValid = false;
+
+                    $contentErr = "Please input text and/or images.";
+                    $f3->set('contentErr', $contentErr);
+                }            }
+            //for ( $i = 0; $i < sizeof($content); $i++ ) {
+           /* foreach($content as $key => $value){
+                foreach ( $value['ops']['insert'] as $item) {
+                    if($item == "\n") {
+                        $isValid = false;
+
+                        $contentErr = "Please input text and/or images.";
+                        $f3->set('contentErr', $contentErr);
+                    }
+                    }
+               // }
+            }
+            /*foreach () {
+                $f3->set('content', $content);
+            } else {
+                $contentErr = "Please input text and/or images.";
+                $f3->set('contentErr', $contentErr);
+                $isValid = false;
+            }*/
+
+            if ($isValid) {
+                $f3->reroute('/view-post');
+            }
+
         }
         echo Template::instance()->render('views/html/home.html');
     });
